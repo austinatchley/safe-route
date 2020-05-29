f68cf6474ff8ea4b3b0cf49b666534f780cb9781
Austin Atchley <austinatchley@gmail.com>
2020-05-29 18:19:23 -0500

    Add interface for testing

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestPostRequests(unittest.TestCase):
+    def test_point(self):
+        r = requests.post(url = "http://127.0.0.1:5000/safety/point", params = {})
+        print r.json()
+        self.assertEqual(r.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
