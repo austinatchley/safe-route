1659e0133fafcb1f88dcd74ef82070b55b3285ef
Austin Atchley <austinatchley@gmail.com>
2020-05-29 19:01:59 -0500

    Add test for route

--- test/test.py
+++ test/test.py
@@ -3,9 +3,13 @@ import requests
 
 class TestPostRequests(unittest.TestCase):
     def test_point(self):
-        r = requests.post(url = "http://127.0.0.1:5000/safety/point", params = {})
-        print r.json()
+        r = requests.post(url = "http://127.0.0.1:5000/safety/point", json = {"location": "test"})
         self.assertEqual(r.status_code, 200)
+
+    def test_route(self):
+        r = requests.post(url = "http://127.0.0.1:5000/safety/route", json = {"locations": ["test1", "test2"]})
+        self.assertEqual(r.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
